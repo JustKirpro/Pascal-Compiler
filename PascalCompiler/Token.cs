2ef09bc06cf47f4502be11599d74edc8d29a4aa2
Kirill Proskuryakov <justkirpro@gmail.com>
2021-10-09 15:33:08 +0500

    Variant and Token classes added

---
+++ PascalCompiler/Token.cs
@@ -0,0 +1,60 @@
+﻿
+namespace PascalCompiler
+{
+    public enum TokenType
+    {
+        Identifier,
+        Operation,
+        Constant
+    }
+
+    public abstract class Token
+    {
+        public TokenType Type { get; protected set; }
+    }
+
+    public class IdentifierToken : Token
+    {
+        public string Identifier { get; }
+
+        public IdentifierToken(string identifier)
+        {
+            Identifier = identifier;
+            Type = TokenType.Identifier;
+        }
+
+        public override string ToString() => $"{Type} | {Identifier}";
+    }
+
+    public class OperationToken : Token
+    {
+        public string Operation { get; }
+
+        public OperationToken(string operation)
+        {
+            Operation = operation;
+            Type = TokenType.Operation;
+        }
+
+        public override string ToString() => $"{Type} | {Operation}";
+    }
+
+    public class ConstantToken : Token
+    {
+        public Variant Variant { get; }
+
+        public ConstantToken(string constant)
+        {
+            if (int.TryParse(constant, out int intValue))
+                Variant = new IntegerVariant(intValue);
+            else if (float.TryParse(constant, out float floatValue))
+                Variant = new FloatVariant(floatValue);
+            else
+                Variant = new StringVariant(constant);
+
+            Type = TokenType.Constant;
+        }
+
+        public override string ToString() => $"{Type} | {Variant}";
+    }
+}
---
+++ PascalCompiler/Variant.cs
@@ -0,0 +1,54 @@
+﻿
+namespace PascalCompiler
+{
+    public enum VariantType
+    {
+        Integer,
+        Float,
+        String
+    }
+
+    public abstract class Variant
+    {
+        public VariantType Type { get; protected set; }
+    }
+
+    public class IntegerVariant : Variant
+    {
+        public int Value { get; }
+
+        public IntegerVariant(int value)
+        {
+            Value = value;
+            Type = VariantType.Integer;
+        }
+
+        public override string ToString() => $"{Type} | {Value}";
+    }
+
+    public class FloatVariant : Variant
+    {
+        public float Value { get; }
+
+        public FloatVariant(float value)
+        {
+            Value = value;
+            Type = VariantType.Float;
+        }
+
+        public override string ToString() => $"{Type} | {Value}";
+    }
+
+    public class StringVariant : Variant
+    {
+        public string Value { get; }
+
+        public StringVariant(string value)
+        {
+            Value = value;
+            Type = VariantType.String;
+        }
+
+        public override string ToString() => $"{Type} | {Value}";
+    }
+}
